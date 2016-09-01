1d7a364fe049466a8efc7a7b71a89729f41b4ac8
ruiStartX <ruiaguiar@startx.com>
2016-09-01 12:33:19 -0700

    Update compile.rb

--- compiler/compile.rb
+++ compiler/compile.rb
@@ -1,6 +1,6 @@
-$LOAD_PATH << File.expand_path("../../twostroke/lib", __FILE__)
+$LOAD_PATH << File.expand_path("../../twostroke/lib", __FILE__);
 require "twostroke"
-require File.expand_path("../jsx_compiler", __FILE__)
+require File.expand_path("../jsx_compiler", __FILE__);
 
 file = File.open ARGV.first, "r"
 parser = Twostroke::Parser.new Twostroke::Lexer.new file.read
